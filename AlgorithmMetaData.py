7ab9544adeeb1c6ca799ef90bc11800b385000ff
Ido Savion <idosavion@gmail.com>
2019-06-01 13:38:24 +0300

    Add algorithm comparator

---
+++ AlgorithmMetaData.py
@@ -0,0 +1,17 @@
+import math
+
+
+class AlgorithmMetaData:
+    def __init__(self, n: int, m: int, k: int, t: int):
+        self.n = n
+        self.m = m
+        self.k = k
+        self.t = t
+        self.w_0 = self._calculate_asymptotic_complexity()
+        self.run_time_func = lambda x: x ** self.w_0
+
+    def _calculate_asymptotic_complexity(self) -> float:
+        return math.log(self.t ** 3, self.n * self.m * self.k)
+
+    def calculate_runtime(self, dim):
+        return self.run_time_func(dim)
